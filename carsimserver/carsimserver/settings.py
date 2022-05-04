be7b443e97bfb3f9f6dad7b8fd80022744a52101
Rodrigo Taipe <rodrigotaipe@dhcp-vl2041-30004.eduroam.cornell.edu>
2022-05-03 23:00:49 -0400

    Fix GCP env var

--- carsimserver/carsimserver/settings.py
+++ carsimserver/carsimserver/settings.py
@@ -24,16 +24,16 @@ env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, '.env')
 if os.path.isfile(env_file):
     env.read_env(env_file)
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+elif os.environ.get("GCP_PROJECT", None):
     # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+    project_id = os.environ.get("GCP_PROJECT")
     client = secretmanager.SecretManagerServiceClient()
     settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/carsimulatorserver-secrets/versions/latest"
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
     env.read_env(io.StringIO(payload))
 else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+    raise Exception("No local .env or GCP_PROJECT detected. No secrets found.")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
